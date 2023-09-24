54e37f405b8a17c562149dc1db104d424472ec47
QuietCraftsman <145918763+QuietCraftsman@users.noreply.github.com>
2023-09-25 02:06:35 +0300

    inquirer-scrollable list of messages.

--- scrubber.mjs
+++ scrubber.mjs
@@ -1,7 +1,9 @@
-import inquirer from 'inquirer';
-import { existsSync, readFileSync, readdirSync, statSync } from 'node:fs';
+import { createReadStream, existsSync, readFileSync, readdirSync, statSync } from 'node:fs';
 import { join } from 'node:path';
 import { exit } from 'node:process';
+
+import csvParser from 'csv-parser';
+import inquirer from 'inquirer';
 
 
 const crawlDataDump = async (dataDumpPath) => {
@@ -26,7 +28,7 @@ const crawlDataDump = async (dataDumpPath) => {
     });
 
     await displayChannelList(dataDumpPath, indexData, subdirectories);
-}
+};
 
 const displayChannelList = async (dataDumpPath, indexData, subdirectories) => {
     const choices = subdirectories.map((subdirectory) => {
@@ -58,10 +60,59 @@ const displayChannelList = async (dataDumpPath, indexData, subdirectories) => {
         exit(0);
     } else {
         // Display messages or further navigation for the selected channel
-        console.log(`Selected: ${selectedChannel}`);
-        // ... Implement logic to display messages or other operations
+        // console.log(`Selected: ${selectedChannel}`);
+        await displayMessages(selectedChannel, dataDumpPath, indexData, subdirectories);
     }
-}
+};
+
+const displayMessages = async (selectedChannel, dataDumpPath, indexData, subdirectories) => {
+    const csvPath = join(dataDumpPath, 'messages', selectedChannel, 'messages.csv');
+
+    if (!existsSync(csvPath)) {
+        console.error('messages.csv not found for the selected channel.');
+
+        await displayChannelList(dataDumpPath, indexData, subdirectories);  // Return to channel listing.
+        return;
+    }
+
+    const messages = [];
+
+    createReadStream(csvPath)
+        .pipe(csvParser())
+        .on('data', (row) => {
+            // Extract ID and Contents only.
+            messages.push({
+                name: `${row.ID}: ${row.Contents}`,
+                value: row.ID
+            });
+        })
+        .on('end', async () => {
+            messages.push(new inquirer.Separator());
+            messages.push({
+                name: "Back to Channel List",
+                value: "back_to_list"
+            });
+
+            const { selectedMessage } = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'selectedMessage',
+                    message: 'Select a message:',
+                    choices: messages,
+                    pageSize: 15
+                }
+            ]);
+
+            if (selectedMessage === "back_to_list") {
+                await displayChannelList(dataDumpPath, indexData, subdirectories);
+            } else {
+                // For now, we just print the selected message ID, but you can expand this section
+                // to display more details or provide further options.
+                console.log(`Selected Message ID: ${selectedMessage}`);
+                await displayMessages(selectedChannel, dataDumpPath, indexData, subdirectories); // Return to message list
+            }
+        });
+};
 
 // Start the script by prompting the user for the data dump path.
 inquirer.prompt([
